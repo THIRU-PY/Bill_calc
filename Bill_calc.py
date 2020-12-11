5fa9eb44dd7d3b35789f3cf803b4d058d03806cd
THIRU-PY <75850683+THIRU-PY@users.noreply.github.com>
2020-12-11 21:29:48 +0530

    Add files via upload

---
+++ Bill_calc.py
@@ -0,0 +1,28 @@
+bill=input("What is the bill amount(INR)\n")
+per=input("How much percentage of tip do you want to give(10,15 or 20)\n")
+Con_bill=int(bill)
+Con_per=int(per)
+if Con_per==10:
+
+    tip=Con_bill*(10/100)
+    amount_to_pay=Con_bill+tip
+    round(amount_to_pay,2)
+    print(f"Amount To Pay = Rs.{amount_to_pay}")
+
+elif Con_per==15:
+
+    tip=Con_bill*(15/100)
+    amount_to_pay=Con_bill+tip
+    round(amount_to_pay,2)
+    print(f"Amount To Pay = Rs.{amount_to_pay}")
+
+elif Con_per==20:
+
+    tip=Con_bill*(20/100)
+    amount_to_pay=Con_bill+tip
+    round(amount_to_pay,2)
+    print(f"Amount To Pay = Rs.{amount_to_pay}")
+
+else:
+
+    print("You can only tip 10 15 or 20 percent of the Con_bill!!")
